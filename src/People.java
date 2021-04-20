1dada72ddc70b4ffabc1afd822278fbd23ab0ea2
morella-1 <2840157105@qq.com>
2021-04-20 15:51:28 +0800

    hello

--- src/People.java
+++ src/People.java
@@ -1,18 +1,14 @@
 public class People {
-    int height;
-    String ear;
-    void speak(String s) {
-        System.out.println(s);
-    }
+   protected double weight,height;
+   public void speakHello(){
+
 }
-class A{
-    public static void main(String args[]){
-        People zhubajie;
-        zhubajie=new People();
-        zhubajie.height=170;
-        zhubajie.ear="两只大耳朵";
-        System.out.println("身高："+zhubajie.height);
-        System.out.println(zhubajie.ear);
-        zhubajie.speak("师傅，咋们别去西天了，改去月宫吧！");
-    }
+public void averageHeight(){
+
+
 }
+public void averageWeight(){
+
+    }
+        }
+
